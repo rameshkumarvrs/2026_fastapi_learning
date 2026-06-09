69a599f2d96c3ce5cd9be11dd00309533949ab31
ramesh <rameshkumarvrs@gmail.com>
2026-06-09 18:11:42 +0530

    changed the url as a quesry parameter

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get('/')
 async def read_root():
     return {"message": "hello world"}
+
+@app.get('/greet')
+async def greetings(name:str) -> dict:
+    return {"message": f"hello {name}"}
