00860603c47c7a968e1e3390b332bc5c773569f7
ramesh <rameshkumarvrs@gmail.com>
2026-06-09 19:40:21 +0530

    added pydantic model and create book model vlaidation class

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 
+from typing import Optional
+
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -8,5 +12,17 @@ async def read_root():
     return {"message": "hello world"}
 
 @app.get('/greet')
-async def greetings(name:str) -> dict:
-    return {"message": f"hello {name}"}
+async def greetings(name: Optional[str]="User", age: int=0) -> dict:
+    return {"message": f"hello {name}", "age": age}
+
+
+class CreateBookModel(BaseModel):
+    title: str
+    author: str
+
+@app.post('/create_book')
+async def book_create(book_data: CreateBookModel):
+    return {
+        "title": book_data.title,
+        "author": book_data.author
+    }
